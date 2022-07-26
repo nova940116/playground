03aa3b161939a9d3dcb6a220a96c88673f4273c1
Nova <nova940116@gmail.com>
2022-07-26 21:26:27 +0900

    VideoPlayer iOS Test

--- vue/src/components/VideoPlayer.vue
+++ vue/src/components/VideoPlayer.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="container">
-    <video id="player" controls>
+  <div class="video-player">
+    <video class="video">
       <source :src="props.src[0]" />
     </video>
+    <div class="controls">
+      <button class="exit" @click="exit">Exit</button>
+    </div>
   </div>
 </template>
 
@@ -10,14 +13,52 @@
 import { onMounted } from "vue"
 const props = defineProps(["src"])
 
+interface Document {
+  requestFullscreen?: () => void
+  webkitRequestFullscreen?: () => void
+  webkitExitFullscreen?: () => void
+  exitFullscreen?: () => void
+}
+
 onMounted(() => {
-  const player = document.getElementById("player")
-  player?.requestFullscreen()
+  const player = document.querySelector(".video-player") as Document
+  if(player?.requestFullscreen) {
+    player?.requestFullscreen()
+  }
+  if(player?.webkitRequestFullscreen) {
+    player?.webkitRequestFullscreen()
+  }
 })
+
+const exit = () => {
+  const doc = document as Document
+  if(doc.exitFullscreen) {
+    doc.exitFullscreen()
+  }
+  if(doc.webkitExitFullscreen) {
+    doc.webkitExitFullscreen()
+  }
+}
 </script>
 
 <style>
-#player {
+* {
+  margin: 0;
+  padding: 0;
+}
+.video-player {
   width: 100vw;
+  height: 100vh;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+}
+.video {
+  width: 100vw;
+  max-height: 90%;
+}
+.controls {
+  height: 40px;
+  margin-top: auto;
 }
 </style>
